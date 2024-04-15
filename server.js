4d44ce21c68f5757af9af93e3a0960ec915af3ba
daisyisibor <daisyisibor8@gmail.com>
2024-04-15 17:19:07 -0400

    created more files

---
+++ server.js
@@ -0,0 +1,42 @@
+// All connections to the PORT 3001 and all requires
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+secret: 'Super secret secret',
+cookie: {maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',},
+resave: false,
+saveUninitialized: true,
+store: new SequelizeStore({
+    db: sequelize
+})
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening v2'));
+});
