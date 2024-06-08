50e306101372edbd601fb36b53420b0de455ebf9
daisyisibor <daisyisibor8@gmail.com>
2024-06-07 22:29:24 -0400

    included partials on the handlebars

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers,
+    partialsDir: path.join(__dirname, 'views/partials')
+ });
 
 const sess = {
 secret: 'Super secret secret',
